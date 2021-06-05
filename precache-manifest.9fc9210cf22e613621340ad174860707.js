83780291a56d74faa18eac976997f79a2271d9b3
Joey <nguyenquocdat.vn@gmail.com>
2021-06-05 23:59:32 +0700

    Updates

---
+++ precache-manifest.9fc9210cf22e613621340ad174860707.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adc7dca614525153281b7cbba463619",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ec9fec19bfb1a582f35",
+    "url": "./static/css/main.594a1e56.chunk.css"
+  },
+  {
+    "revision": "9425e600b72be24bd567",
+    "url": "./static/js/2.14d80cae.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.14d80cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec9fec19bfb1a582f35",
+    "url": "./static/js/main.fd45c62c.chunk.js"
+  },
+  {
+    "revision": "21f4d74f0712ac329c7c",
+    "url": "./static/js/runtime-main.a4a2d046.js"
+  },
+  {
+    "revision": "7781e69065ec951499613843428ba5fb",
+    "url": "./static/media/logo.7781e690.svg"
+  }
+]);
